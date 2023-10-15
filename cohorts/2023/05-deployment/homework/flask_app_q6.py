ca7ee5f6a62e775b98ef7e5e843175c2032be544
Maksim Pozdniakov <pozdnyakovmaks99@mail.ru>
2023-10-15 23:20:34 +0500

    hw5

---
+++ cohorts/2023/05-deployment/homework/flask_app_q6.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask('predict') # give an identity to your web service
+
+
+with open('model2.bin', 'rb') as f_model:
+    model = pickle.load(f_model)
+with open("dv.bin", "rb") as f_dv: 
+    dv = pickle.load(f_dv)
+
+@app.route('/predict', methods=['POST']) # use decorator to add Flask's functionality to our function
+def predict():
+    
+    client = request.get_json()
+
+    proba = model.predict_proba(dv.transform([client]))[:,1][0]
+    
+    result = {"proba" : float(proba), "class": bool(proba>=0.5)}
+    return jsonify(result)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
+    
